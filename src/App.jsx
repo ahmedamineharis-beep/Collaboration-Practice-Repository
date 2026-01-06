cebab8e4892fddadc2258a913b8c208a3ca14558
ahmed amine haris <ahmedamineharis@gmail.com>
2026-01-06 18:53:39 +0100

    link home with app and link navbar,body and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Home from './src/Pages/Home'
 import './App.css'
 
 function App() {
@@ -6,7 +7,7 @@ function App() {
 
   return (
     <>
-
+      <Home />
     </>
   )
 }
